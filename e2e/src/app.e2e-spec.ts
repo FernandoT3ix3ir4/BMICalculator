22250b9a33b45662d2e03a5b9cb04970e59d38e9
Fernando Teixeira <e.fernandots@gmail.com>
2021-02-10 21:22:35 -0300

    atualizacao para angular 10

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to BMICalculator!');
+    expect(page.getTitleText()).toEqual('bmi-calculator app is running!');
   });
 
   afterEach(async () => {
